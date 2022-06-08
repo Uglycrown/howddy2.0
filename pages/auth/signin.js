b8ba9871cc4fac1fd2f86850e5d0308c9eb6bebc
Gourav Singh <87015615+Uglycrown@users.noreply.github.com>
2022-06-08 13:14:13 +0530

    Update signin.js

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -12,7 +12,7 @@ function signin({ providers }) {
       <div className=" mt-40">
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id, {callbackUrl: "https://howddy2-0-p1rvtvmr4-uglycrown.vercel.app"})}>
+          <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id, {callbackUrl: "/"})}>
             Sign in with {provider.name}
           </button>
         </div>
